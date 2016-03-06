256ac63b8dbb6681889e26329a5648bbdf8787f8
Sijie Tian <sijie@socialtables.com>
2016-03-06 14:44:14 -0500

    added module.exports and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+## ins-goo-vision
+
+A module that user can pass their instagram API key and google vision API key and return the results of each of your image in your instagram.
+
+Part of the results
+![alt text](./ins-goo-vision.png)
--- index.js
+++ index.js
@@ -2,53 +2,56 @@ require('dotenv').config();
 var request = require('request');
 var async = require('async');
 
-var results = [];
-var instagramURL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + process.env.INS_ACCESS_TOKEN;
-request.get({url: instagramURL}, function(err, res, body) {
-  body = JSON.parse(body);
+module.exports = function(INS_ACCESS_TOKEN, GOO_VISION_KEY, callback) {
+  callback = callback || function() {};
+  var results = [];
+  var instagramURL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + process.env.INS_ACCESS_TOKEN || INS_ACCESS_TOKEN;
+  request.get({url: instagramURL}, function(err, res, body) {
+    body = JSON.parse(body);
 
-  async.each(body.data, sendIns2GooVision, function(err) {
-    if (err) {
-      console.error(err);
-    }
-    console.log(results);
-  });
-});
-
-
-function sendIns2GooVision(ins, callback) {
-  var imageURL = ins.images.standard_resolution.url;
-  request.get({url: imageURL, encoding: 'base64'}, function (err, res, body) {
-    var data = {
-      "requests":[
-        {
-          "image":{
-            "content": body
-          },
-          "features":[
-            {
-              "type":"LABEL_DETECTION",
-              "maxResults":2
-            }
-          ]
-        }
-      ]
-    };
-
-    var options = {
-         url: 'https://vision.googleapis.com/v1/images:annotate?key=' + process.env.GOO_VISION_KEY,
-         headers: {'Content-Type': 'application/json'},
-         json: data
-    };
-
-    request.post(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        results.push(body.responses[0].labelAnnotations)
-        callback();
+    async.each(body.data, sendIns2GooVision, function(err) {
+      if (err) {
+        console.error(err);
       }
-      else {
-        callback(error);
-      }
+      callback(results);
     });
   });
+
+
+  function sendIns2GooVision(ins, cb) {
+    var imageURL = ins.images.standard_resolution.url;
+    request.get({url: imageURL, encoding: 'base64'}, function (err, res, body) {
+      var data = {
+        "requests":[
+          {
+            "image":{
+              "content": body
+            },
+            "features":[
+              {
+                "type":"LABEL_DETECTION",
+                "maxResults":2
+              }
+            ]
+          }
+        ]
+      };
+
+      var options = {
+           url: 'https://vision.googleapis.com/v1/images:annotate?key=' + process.env.GOO_VISION_KEY || GOO_VISION_KEY,
+           headers: {'Content-Type': 'application/json'},
+           json: data
+      };
+
+      request.post(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          results.push(body.responses[0].labelAnnotations)
+          cb();
+        }
+        else {
+          cb(error);
+        }
+      });
+    });
+  }
 }
